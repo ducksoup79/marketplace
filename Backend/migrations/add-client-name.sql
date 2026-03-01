a9344d4245bb1cb70b740eaaf41395794b3fb191
Joe <flyingnut101@gmail.com>
2026-03-01 10:38:36 +0200

    Delete Backend directory

--- Backend/migrations/add-client-name.sql
+++
@@ -1,3 +0,0 @@
--- Add first_name and last_name to client (run if your DB was created before this change)
-ALTER TABLE client ADD COLUMN IF NOT EXISTS first_name VARCHAR(100);
-ALTER TABLE client ADD COLUMN IF NOT EXISTS last_name VARCHAR(100);
